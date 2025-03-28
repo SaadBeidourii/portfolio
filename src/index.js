22efe4648f302290fd623433373b947f758e27b5
BOITI Aymane <127843983+AymaneBOITI@users.noreply.github.com>
2025-03-28 04:17:55 +0100

    update router
    Get the base name from the homepage in package.json or default to '/'

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+const basename = process.env.PUBLIC_URL || '/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
